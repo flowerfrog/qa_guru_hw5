21f24c30c2cbba9356e250da70a763e2d6298de0
Alexandra Osennyaya <alexosenn@gmail.com>
2023-10-02 13:35:29 +0300

    replacing the absolute path to the image with a universal one editing a completed form check removed image in /tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,10 +7,10 @@ def setup():
     print("\nstart browser for test..")
     browser.config.window_width = 1920
     browser.config.window_height = 1200
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 2.0
     driver_options = webdriver.ChromeOptions()
-    #driver_options.add_argument('--headless=new')
+    driver_options.add_argument('--headless=new')
     browser.config.driver_options = driver_options
     yield browser
     print("\nquit browser..")
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,8 +1,9 @@
 from selene import browser, have, command
+import os
 
 
 def test_complete_practice_form():
-    browser.open('/')
+    browser.open('/automation-practice-form')
     browser.element('#firstName').type('test')
     browser.element('#lastName').type('user')
     browser.element('#userEmail').type('test@user.com')
@@ -13,8 +14,8 @@ def test_complete_practice_form():
     browser.element('.react-datepicker__year-select').type('2000')
     browser.element('.react-datepicker__day.react-datepicker__day--025').click()
     browser.element('#subjectsInput').type('math').press_enter()
-    browser.element('//*[@id="hobbiesWrapper"]/div[2]/div[2]/label').click()
-    browser.element('//input[@type="file"]').send_keys('/home/user/Изображения/image.jpg')
+    browser.element('label[for="hobbies-checkbox-2"]').click()
+    browser.element('//input[@type="file"]').send_keys(os.path.abspath("image.jpg"))
     browser.element('#currentAddress').type('Ростов-сити, Забугорная 3к1')
     browser.element('//*[@id="react-select-3-input"]').type('NCR').press_enter()
     browser.element('//*[@id="react-select-4-input"]').type('Delhi').press_enter()
@@ -22,7 +23,16 @@ def test_complete_practice_form():
     browser.all('#userForm > div').should(have.size(11))
     browser.element('#submit').click()
     browser.all('#todo-list>li').by(have.css_class('was-validated'))
-    browser.all('div.modal-body > div > table > tbody > tr').should(have.size(10))
+    browser.element("//tbody/tr[1]").should(have.text("test user"))
+    browser.element("//tbody/tr[2]").should(have.text("test@user.com"))
+    browser.element("//tbody/tr[3]").should(have.text("Female"))
+    browser.element("//tbody/tr[4]").should(have.text("Mobile 7999999999"))
+    browser.element("//tbody/tr[5]").should(have.text("Date of Birth 25 December,2000"))
+    browser.element("//tbody/tr[6]").should(have.text("Math"))
+    browser.element("//tbody/tr[7]").should(have.text("Reading"))
+    browser.element("//tbody/tr[8]").should(have.text("image.jpg"))
+    browser.element("//tbody/tr[9]").should(have.text("Ростов-сити, Забугорная 3к1"))
+    browser.element("//tbody/tr[10]").should(have.text("NCR Delhi"))
 
 
 
